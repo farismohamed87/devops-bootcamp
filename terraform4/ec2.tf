fb6453f56b82dd1bd96dd03e19349233a96e2e44
farismohamed87 <mrpayeh@gmail.com>
2026-08-06 22:43:36 +0800

    add terraform 4

--- terraform4/ec2.tf
+++ terraform4/ec2.tf
@@ -16,7 +16,7 @@ module "my_rackula_server" {
   ami                    = data.aws_ami.my_ami.id
   instance_type          = "t3.micro"
   subnet_id              = module.my_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.my_sg.security_group_id]
+  vpc_security_group_ids = [module.my_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.rackula.name
 
   create_security_group = false
